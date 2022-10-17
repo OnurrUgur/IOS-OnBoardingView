c0586d63811c19c6b6f741172ec14d7acf747671
OnurrUgur <onurugur.prf@gmail.com>
2022-10-17 22:39:51 +0300

    Initial Commit

---
+++ zodica/zodicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  zodicaApp.swift
+//  zodica
+//
+//  Created by Onur Uğur on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct zodicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
